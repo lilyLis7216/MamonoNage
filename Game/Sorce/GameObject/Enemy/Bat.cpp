be985a43ef29140fa4ac73ad24bec42926d893b0
406-10 <>
2023-01-31 12:22:43 +0900

    batのモーションと画像追加

--- Game/Sorce/GameObject/Enemy/Bat.cpp
+++ Game/Sorce/GameObject/Enemy/Bat.cpp
@@ -14,6 +14,14 @@ Bat::~Bat()
 {
 }
 
+void Bat::Init(VECTOR Pos)
+{
+    pos = Pos;
+    LoadDivGraph("../asset/enemy/bat/move.jpg", MoveAllNum, XNum, YNum, XSize, YSize, mMove);
+    LoadDivGraph("../asset/enemy/bat/attack.jpg", MoveAllNum, XNum, YNum, XSize, YSize, mMove);
+    LoadDivGraph("../asset/enemy/bat/damage.jpg", MoveAllNum, DamageXNum, YNum, XSize, YSize, mMove);
+
+}
 /// <summary>
 /// コウモリの更新処理
 /// </summary>
@@ -25,6 +33,7 @@ void Bat::Update(float _deltaTime)
 /// <summary>
 /// コウモリの描画処理
 /// </summary>
-void Bat::Draw(int none1, int none2)
+void Bat::Draw(int offSetX, int offSetY)
 {
+    DrawRotaGraph((int)pos.x - offSetX, (int)pos.y - offSetY, 2, 0, handle, TRUE, rightDir);
 }
